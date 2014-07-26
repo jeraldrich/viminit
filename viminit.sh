c89014503971afcac68246701ec4f8edaecf1844
Jeremy Aldrich <jeremy@jeremyaldrich.net>
2014-07-26 13:49:38 -0500

    Update viminit.sh
    show special chars for tabs only in python (annoying in go and others)

--- viminit.sh
+++ viminit.sh
@@ -107,7 +107,7 @@ modify_vimrc "let g:EasyGrepFilesToExclude = '*.pyc,*.rbc'"
 # backspace
 modify_vimrc "set backspace=indent,eol,start"
 # show whitespace characters
-modify_vimrc "set list listchars=tab:\\|\\" 
+modify_vimrc "autocmd filetype python set list listchars=tab:\\|\\" 
 modify_vimrc "highlight Whitespace cterm=underline gui=underline ctermbg=NONE guibg=NONE ctermfg=DarkGray guifg=DarkGray"
 modify_vimrc "autocmd ColorScheme * highlight Whitespace gui=underline ctermbg=NONE guibg=NONE ctermfg=DarkGray guifg=DarkGray"
 modify_vimrc "match Whitespace /  \\+/"
